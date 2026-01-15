1aedb0559eef04642a19bb4faf184c88a6b50a8c
Serena <serenazhou4268@gmail.com>
2026-01-15 12:32:22 -0500

    main

---
+++ create-project/src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+// selected array of recipes
+const recipe = [
+  {
+    name: "",
+    artist: "",
+    published: "",
+    img: "",
+    alt: "",
+    category: "",
+  },
+]
